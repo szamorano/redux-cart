5774f0d80f4cef2d23b8f978fa328e6cdea33def
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-07-31 16:53:14 -0400

    Show and hide cart when My Cart button is pressed.

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import uiSlice from ".ui-slice";
+import uiSlice from "./ui-slice";
 
 const store = configureStore({
   reducer: { ui: uiSlice.reducer },
