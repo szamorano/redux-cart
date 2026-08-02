f3de032c6abcec7edc3d7fb8ecccbbd1404970a0
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-02 14:18:41 -0400

    Cancel triggering the useEffect that sends the cart data again.

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -46,7 +46,10 @@ export function sendCartData(cart) {
         "https://reduxcartdemo-413f1-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         },
       );
 
