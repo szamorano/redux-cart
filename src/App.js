efe76a49f3f3b30fba798c9a42454e0b1fb58f30
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-02 11:44:35 -0400

    Create sendCartData, an action creator thunk, that delays an action until later.

--- src/App.js
+++ src/App.js
@@ -15,49 +15,14 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        }),
-      );
-      const response = await fetch(
-        "https://reduxcartdemo-413f1-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        },
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        }),
-      );
-    };
+    const sendCartData = async () => {};
 
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!!",
-          message: "Sending cart data failed!",
-        }),
-      );
-    });
+    sendCartData().catch((error) => {});
   }, [cart, dispatch]);
 
   return (
