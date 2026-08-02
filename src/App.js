6299211158b0a6760feb8de3dc365faf9a9872f4
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-02 13:22:17 -0400

    Created cart-actions.js to fetch cart data.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useEffect, Fragment } from "react";
 import Notification from "./components/UI/Notification";
-import { sendCartData } from "./store/cart-slice";
+import { sendCartData } from "./store/cart-actions";
 
 let isInitial = true;
 
